d71f81fa338f1f0b8e616091c9dede3248ab0190
hw5ujv <89873159+hw5ujv@users.noreply.github.com>
2023-04-15 10:50:44 -0400

    add 3 basic template and config url

--- freshlyLocal/urls.py
+++ freshlyLocal/urls.py
@@ -2,8 +2,11 @@ from django.urls import path, include
 from . import views
 import debug_toolbar
 
-#URl config
+# URl config
 urlpatterns = [
     path('hello/', views.say_hello),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
