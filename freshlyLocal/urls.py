4e0e5de485e35293ed0255feea2788d559059502
hw5ujv <89873159+hw5ujv@users.noreply.github.com>
2023-04-15 12:16:47 -0400

    integrate main page

--- freshlyLocal/urls.py
+++ freshlyLocal/urls.py
@@ -6,20 +6,20 @@ import debug_toolbar
 #<<<<<<< HEAD
 #URl config
 app_name = 'freshlyLocal'
-urlpatterns = [
-    path('hello/', views.say_hello),
-    path('', views.store, name="store"),
-    path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout"),
-
-]
+# urlpatterns = [
+#     path('hello/', views.say_hello),
+#     path('', views.store, name="store"),
+#     path('cart/', views.cart, name="cart"),
+#     path('checkout/', views.checkout, name="checkout"),
+#
+# ]
 #=======
 ## URl config
-#urlpatterns = [
-#    path('hello/', views.say_hello),
-#    path('__debug__/', include('debug_toolbar.urls')),
-#    path('', views.store, name="store"),
-#    path('cart/', views.cart, name="cart"),
-#    path('checkout/', views.checkout, name="checkout"),
-#]
+urlpatterns = [
+   path('hello/', views.say_hello),
+   path('__debug__/', include('debug_toolbar.urls')),
+   path('', views.store, name="store"),
+   path('cart/', views.cart, name="cart"),
+   path('checkout/', views.checkout, name="checkout"),
+]
 #>>>>>>> e7f4ef01f931343f7a13f0cbbf9d5a3af6236423
